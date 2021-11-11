cabb4f3835829499c6968b5d9671a9bbfd1bd6e2
Alejandra De Los Santos <delossantosa@jefatura.gob.ar>
2021-11-11 16:31:59 -0300

    minor changes

--- routes/recipesRouter.js
+++ routes/recipesRouter.js
@@ -7,5 +7,6 @@ const { validateId } = require('../middlewares/actions/generic');
 router.get('/all', all);
 router.post('/create', validateCreate, create);
 router.get('/:id', find);
+router.get('/', find);
 
 module.exports = router;
